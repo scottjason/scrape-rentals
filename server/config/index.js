7b0e080dcfca7abf4fd819a9f22932d633454117
Scott Jason <scottleejason@gmail.com>
2015-07-25 16:26:45 -0700

    updates config vars

--- server/config/index.js
+++ server/config/index.js
@@ -4,25 +4,11 @@
 
 'use strict';
 
-var env = {};
 var path = require('path');
-
-if (process.env.NODE_ENV !== 'production') {
-  env = require('../../env.json');
-}
 
 module.exports = {
   server: {
     port: process.env.PORT || 3000
   },
-  root: path.normalize(__dirname + '../../../'),
-  sessionOpts: {
-    saveUninitialized: true,
-    resave: true,
-    secret: process.env.SESSION_SECRET || env.SESSION_SECRET,
-    cookie: {
-      maxAge: new Date(Date.now() + 1209600000),
-      expires: new Date(Date.now() + 1209600000)
-    }
-  }
+  root: path.normalize(__dirname + '../../../')
 };
